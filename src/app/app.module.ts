8d7a93464f340f774494a888e1135b3196beee8b
Mohammed Arqam <mohammedarqam97@gmail.com>
2018-12-05 22:19:12 +0530

    Libs added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,23 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
-
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import * as firebase from 'firebase';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { ChartsModule } from 'ng2-charts';
+
+export const firebaseCred = {
+  apiKey: "AIzaSyDCES9tQHEU81poCBxTkcxOHe7ENgEUDhM",
+  authDomain: "ebani-survey.firebaseapp.com",
+  databaseURL: "https://ebani-survey.firebaseio.com",
+  projectId: "ebani-survey",
+  storageBucket: "",
+  messagingSenderId: "803503781446"
+};
+firebase.initializeApp(firebaseCred);
+
 
 @NgModule({
   declarations: [
@@ -14,7 +28,10 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseCred),
+    AngularFireDatabaseModule,
+    ChartsModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, LoadingController } from 'ionic-angular';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,24 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(
+  public navCtrl: NavController,
+  public loadingCtrl : LoadingController, 
+  ) {
+    this.getResponses();
   }
 
+  getResponses(){
+    let loading = this.loadingCtrl.create({
+      content: 'Please wait...'
+    });
+
+    firebase.database().ref("Responses").once("value",itemSnap=>{
+      itemSnap.forEach(snap=>{
+        console.log(snap.val());
+      })
+      loading.dismiss();
+    })
+
+  }
 }
